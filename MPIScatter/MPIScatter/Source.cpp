e9c2edf22fa958980cbaa7e3f2d6da9ea8ea3364
moudroff <moudroff>
2018-04-02 10:57:47 +0300

    Scatter

--- MPIScatter/MPIScatter/Source.cpp
+++ MPIScatter/MPIScatter/Source.cpp
@@ -1,32 +1,64 @@
 #include <iostream>
 #include <mpi.h>
-//#include <time.h>
+#include <time.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
 	const int N = 20;
-	int rank, size, procN;
-	double a[N], *procA, sum = 0, sum1 = 0, procSum = 0;
+	int rank, size;
+	double a[N], sum = 0, sum1 = 0, procSum = 0;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	//srand();
-	if (!rank) // rank == 0
+	srand(time(0));
+	if (!rank)
 	{
 		for (int i = 0; i < N; i++)
 		{
-			a[i] = (double)(rand() % 100) / 100;
+			a[i] = rand() % 100;
 			printf("%f ", a[i]);
 			sum1 += a[i];
 		}
-		printf("\n");
-
 	}
 
+	//MPI_Bcast(a, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	int *len = new int[size];
+	int *ind = new int[size];
+
+	int rest = N;
+	int k = rest / size;
+	len[0] = k;
+	ind[0] = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		rest -= k;
+		k = rest / (size - i);
+		len[i] = k;
+		ind[i] = ind[i - 1] + len[i - 1];
+	}
+
+	int nProc = len[rank];
+	double *aProc = new double[nProc];
+
+	MPI_Scatterv(a, len, ind, MPI_DOUBLE, aProc, nProc,
+		MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	procSum = 0;
+	for (int i = 0; i < nProc; i++)
+	{
+		//printf("rank - %d  a[%d]=5%d ",rank,i,aProc[i]);
+		procSum += aProc[i];
+	}
+	MPI_Reduce(&procSum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if (!rank)
+		printf("\nsum=%f sum1=%f\n", sum, sum1);
 
 	MPI_Finalize();
 
